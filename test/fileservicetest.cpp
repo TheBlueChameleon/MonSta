a94fb9507a1de9e8eee4ec6aa20fecefdd56e753
Stefan Hartinger <stefan_hartinger@gmx.de>
2026-03-26 20:57:38 +0100

    sets up test framework

---
+++ test/fileservicetest.cpp
@@ -0,0 +1,9 @@
+#include "fileservicetest.hpp"
+
+FileServiceTest::FileServiceTest()
+{}
+
+TEST_F(FileServiceTest, Foo)
+{
+    ASSERT_TRUE(false) << "otherwise";
+}
---
+++ test/fileservicetest.hpp
@@ -0,0 +1,12 @@
+#ifndef FILESERVICETEST_HPP
+#define FILESERVICETEST_HPP
+
+#include <gtest/gtest.h>
+
+class FileServiceTest : public testing::Test
+{
+    protected:
+        FileServiceTest();
+};
+
+#endif // FILESERVICETEST_HPP
