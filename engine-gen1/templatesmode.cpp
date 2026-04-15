daf0f9a5b0171beceff44bc5ec8ea1ce7d7487a9
Stefan Hartinger <stefan_hartinger@gmx.de>
2026-04-15 18:34:05 +0200

    defines structure for client template mode

--- engine-gen1/templatesmode.cpp
+++ engine-gen1/templatesmode.cpp
@@ -9,19 +9,132 @@
 
 namespace TemplateMode
 {
-    void writeSchemas()
+    static void writeSchemas()
     {
         auto handleTeamDef = JsonService::get(SchemaValidation::JTAG_TEAMDEFINITION);
-        auto content = JsonService::dump(handleTeamDef);
-        FileService::write(SchemaValidation::filename_teamDefinition, content.getAsStringView());
+        auto contentTeamDef = JsonService::dump(handleTeamDef);
+        FileService::write(SchemaValidation::filename_teamDefinition, contentTeamDef.getAsStringView());
 
-        auto handleXDef = JsonService::get(SchemaValidation::JTAG_MECHANICSDEFINITION);
-        auto contentX = JsonService::dump(handleXDef);
-        FileService::write(SchemaValidation::filename_mechanicsDefinition, contentX.getAsStringView());
+        auto handleMechanicsDef = JsonService::get(SchemaValidation::JTAG_MECHANICSDEFINITION);
+        auto contentMechanicsDef = JsonService::dump(handleMechanicsDef);
+        FileService::write(SchemaValidation::filename_mechanicsDefinition, contentMechanicsDef.getAsStringView());
+    }
+
+    static std::string writeMechanicsDefinitionFile(
+        const std::string inputName,
+        const std::string defaultName
+    )
+    {
+        if (!inputName.empty())
+        {
+            return inputName;
+        }
+
+
+        return defaultName;
+    }
+
+    static std::string writeTeamDefinitionFile(
+        const std::string inputName,
+        const std::string defaultName
+    )
+    {
+        if (!inputName.empty())
+        {
+            return inputName;
+        }
+
+
+        return defaultName;
+    }
+
+    static std::string writeStrategyFile(
+        const std::string inputName,
+        const std::string defaultName
+    )
+    {
+        if (!inputName.empty())
+        {
+            return inputName;
+        }
+
+
+        return defaultName;
+    }
+
+    static std::string writePkmnDefinitionFile(
+        const std::string inputName,
+        const std::string defaultName
+    )
+    {
+        if (!inputName.empty())
+        {
+            return inputName;
+        }
+
+
+        return defaultName;
+    }
+
+    static std::string writeMoveDefinitionFile(
+        const std::string inputName,
+        const std::string defaultName
+    )
+    {
+        if (!inputName.empty())
+        {
+            return inputName;
+        }
+
+
+        return defaultName;
+    }
+
+    static std::string writeTypeDefinitionFile(
+        const std::string inputName,
+        const std::string defaultName
+    )
+    {
+        if (!inputName.empty())
+        {
+            return inputName;
+        }
+
+
+        return defaultName;
+    }
+
+    static std::string writeItemDefinitionFile(
+        const std::string inputName,
+        const std::string defaultName
+    )
+    {
+        if (!inputName.empty())
+        {
+            return inputName;
+        }
+
+
+        return defaultName;
+    }
+
+    static void writeAllowedValueFile()
+    {
+
     }
 
     void run(const ITemplatesDefinition& templatesDefinition)
     {
+        const auto mechanicsDefinitionFile = writeMechanicsDefinitionFile(templatesDefinition.mechanicsDefinition, MECHANICS_DEFINITION_FILE);
+        const auto player1TeamDefinitionFile = writeTeamDefinitionFile(templatesDefinition.player1Team, PLAYER1_TEAMDEFINITION_FILE);
+        const auto player2TeamDefinitionFile = writeTeamDefinitionFile(templatesDefinition.player2Team, PLAYER2_TEAMDEFINITION_FILE);
+        const auto player1StrategyFile = writeTeamDefinitionFile(templatesDefinition.player1Team, PLAYER1_STRATEGY_FILE);
+        const auto player2StrategyFile = writeTeamDefinitionFile(templatesDefinition.player2Team, PLAYER2_STRATEGY_FILE);
+        const auto pkmnDefinitionFile = writePkmnDefinitionFile(templatesDefinition.pkmnDefs, PKMN_DEFINITION_FILE);
+        const auto moveDefinitionFile = writeMoveDefinitionFile(templatesDefinition.moveDefs, MOVE_DEFINITION_FILE);
+        const auto typeDefinitionFile = writeTypeDefinitionFile(templatesDefinition.typeDefs, TYPE_DEFINITION_FILE);
+        const auto itemDefinitionFile = writeItemDefinitionFile(templatesDefinition.itemDefs, ITEM_DEFINITION_FILE);
+
         if (templatesDefinition.writeSchemas)
         {
             writeSchemas();
--- engine-gen1/templatesmode.hpp
+++ engine-gen1/templatesmode.hpp
@@ -5,6 +5,17 @@ struct ITemplatesDefinition;
 
 namespace TemplateMode
 {
+    const auto MECHANICS_DEFINITION_FILE   = "mechanics.json";
+    const auto PLAYER1_TEAMDEFINITION_FILE = "player1Team.json";
+    const auto PLAYER2_TEAMDEFINITION_FILE = "player2Team.json";
+    const auto PLAYER1_STRATEGY_FILE       = "player1Strategy.lua";
+    const auto PLAYER2_STRATEGY_FILE       = "player2Strategy.lua";
+    const auto PKMN_DEFINITION_FILE        = "pokemonDefinitions.csv";
+    const auto MOVE_DEFINITION_FILE        = "moveDefinitions.csv";
+    const auto TYPE_DEFINITION_FILE        = "typeDefinitions.csv";
+    const auto ITEM_DEFINITION_FILE        = "itemDefinitions.csv";
+    const auto ALLOWED_VALUES_FILE         = "allowedValues.txt";
+
     void run(const ITemplatesDefinition& templatesDefinition);
 }
 
