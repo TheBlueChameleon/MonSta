3137d634f1a909ba5a45930ae78548bbc1386c1f
Stefan Hartinger <stefan_hartinger@gmx.de>
2026-04-21 22:30:38 +0200

    improves parsing error message

--- engine-gen1/simulationmode/setup.cpp
+++ engine-gen1/simulationmode/setup.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <span>
+#include <unordered_map>
 
 #include <base/enginebase.hpp>
 
@@ -55,23 +56,23 @@ namespace SimulationMode
 
     static bool getHumanStatus(const IJsonServiceTypes::JsonHandle handle)
     {
-    // *INDENT-OFF*
-    if      (JsonService::contains(handle, JKEY_HUMAN))    { return true; }
-    else if (JsonService::contains(handle, JKEY_COMPUTER)) { return false; }
-    else {
-        throw EngineError(
-            "team definition neither contains '"s + JKEY_HUMAN + "' tag "
-                                                                 "nor '" + JKEY_COMPUTER + "' tag");
-    }
-    // *INDENT-ON*
+        // *INDENT-OFF*
+        if      (JsonService::contains(handle, JKEY_HUMAN))    { return true; }
+        else if (JsonService::contains(handle, JKEY_COMPUTER)) { return false; }
+        else {
+            throw EngineError(
+                "team definition neither contains '"s + JKEY_HUMAN + "' tag "
+                                                                     "nor '" + JKEY_COMPUTER + "' tag");
+        }
+        // *INDENT-ON*
     }
 
     static const char* const getPlayerBaseObjectName(const bool isHuman)
     {
-    // *INDENT-OFF*
-    if (isHuman) { return JKEY_HUMAN; }
-    else         { return JKEY_COMPUTER; }
-    // *INDENT-ON*
+        // *INDENT-OFF*
+        if (isHuman) { return JKEY_HUMAN; }
+        else         { return JKEY_COMPUTER; }
+        // *INDENT-ON*
     }
 
     static std::string getPlayerObjectJsonPointer(const bool isHuman, std::string_view element)
@@ -157,13 +158,13 @@ namespace SimulationMode
         result.dv_SPC       = getInt(pokemonHandle, JKEY_POKEMON_DVSPC);
         result.dv_SPD       = getInt(pokemonHandle, JKEY_POKEMON_DVSPD);
 
-    // *INDENT-OFF*
-    if (result.dv_HP  == -1) { result.dv_HP  = (humanMode ? 15 : 8); }
-    if (result.dv_ATK == -1) { result.dv_ATK = (humanMode ? 15 : 9); }
-    if (result.dv_DEF == -1) { result.dv_DEF = (humanMode ? 15 : 8); }
-    if (result.dv_SPC == -1) { result.dv_SPC = (humanMode ? 15 : 8); }
-    if (result.dv_SPD == -1) { result.dv_SPD = (humanMode ? 15 : 8); }
-    // *INDENT-ON*
+        // *INDENT-OFF*
+        if (result.dv_HP  == -1) { result.dv_HP  = (humanMode ? 15 : 8); }
+        if (result.dv_ATK == -1) { result.dv_ATK = (humanMode ? 15 : 9); }
+        if (result.dv_DEF == -1) { result.dv_DEF = (humanMode ? 15 : 8); }
+        if (result.dv_SPC == -1) { result.dv_SPC = (humanMode ? 15 : 8); }
+        if (result.dv_SPD == -1) { result.dv_SPD = (humanMode ? 15 : 8); }
+        // *INDENT-ON*
 
         result.statusExperience_HP  = getInt(pokemonHandle, JKEY_POKEMON_STATEXPHP);
         result.statusExperience_ATK = getInt(pokemonHandle, JKEY_POKEMON_STATEXPATK);
@@ -264,11 +265,10 @@ namespace SimulationMode
         }
     }
 
-
     // ---------------------------------------------------------------------- //
     // types definition
 
-    void assertTypeDataCompleteAndGetTypeNames(ICsvService::CsvHandle handle)
+    void assertTypeChartDataComplete(ICsvService::CsvHandle handle)
     {
         // *INDENT-OFF*
         if (!CsvService::hasColumn(handle, TypeChart::ATTACKER)) { throw EngineError("Missing column: "s + TypeChart::ATTACKER); }
@@ -289,6 +289,43 @@ namespace SimulationMode
         CsvService::freeColumnBuffer(typeNames);
     }
 
+    void transferToTypeChart(ICsvService::CsvHandle handle)
+    {
+        const size_t rowCount = CsvService::getRowCount(handle);
+        ICsvService::RowData rowBuffer = CsvService::reserveRowBuffer(handle);
+        CsvService::getRow(handle, rowBuffer, 0);
+
+        auto rowCellView = std::span<ICsvService::CellData>(
+                               rowBuffer.data + 2,                // skip the "Attacker" and "Category" entries
+                               rowBuffer.data + rowBuffer.size
+                           );
+        auto rowStringView = std::vector<std::string_view>(rowCellView.size());
+        const auto extract = [](const ICsvService::CellData& cell)
+        {
+            return cell.data;
+        };
+        std::transform(rowCellView.begin(), rowCellView.end(),
+                       rowStringView.begin(),
+                       extract
+                      );
+        typeChart.setupTypes(rowStringView);
+
+        for (size_t i = 1; i < rowCount; ++i)
+        {
+            CsvService::getRow(handle, rowBuffer, i);       // also updates rowCellView
+            const auto rowName = rowBuffer.data[0].data;
+            const auto categoryName = rowBuffer.data[1].data;
+            const auto category = getMoveCategoryFromName(categoryName);
+            std::transform(rowCellView.begin(), rowCellView.end(),
+                           rowStringView.begin(),
+                           extract
+                          );
+            typeChart.setRow(rowName, category, rowStringView);
+        }
+
+        CsvService::freeRowBuffer(rowBuffer);
+    }
+
     void loadAndRegisterTypesDefinition(
         const std::filesystem::path& typeDefinitionFile,
         ErrorBuffer& eb
@@ -299,49 +336,184 @@ namespace SimulationMode
             LoggerService::traceF("  ... loading types definition from '{}'", typeDefinitionFile.c_str());
 
             ICsvService::CsvHandle csvHandle = CsvService::readCsvData(typeDefinitionFile, ICsvService::CsvOptions{});
-            assertTypeDataCompleteAndGetTypeNames(csvHandle);
-
-            size_t rowCount = CsvService::getRowCount(csvHandle);
-            ICsvService::RowData rowBuffer = CsvService::reserveRowBuffer(csvHandle);
-            CsvService::getRow(csvHandle, rowBuffer, 0);
-
-            auto rowCellView = std::span<ICsvService::CellData>(
-                                   rowBuffer.data + 2,                // skip the "Attacker" and "Category" entries
-                                   rowBuffer.data + rowBuffer.size
-                               );
-            auto rowStringView = std::vector<std::string_view>(rowCellView.size());
-            const auto extract = [](const ICsvService::CellData& cell)
-            {
-                return cell.data;
-            };
-            std::transform(rowCellView.begin(), rowCellView.end(),
-                           rowStringView.begin(),
-                           extract
-                          );
-            typeChart.setupTypes(rowStringView);
-
-            for (size_t i = 1; i < rowCount; ++i)
-            {
-                CsvService::getRow(csvHandle, rowBuffer, i);
-                const auto rowName = rowBuffer.data[0].data;
-                const auto categoryName = rowBuffer.data[1].data;
-                const auto category = getMoveCategoryFromName(categoryName);
-                std::transform(rowCellView.begin(), rowCellView.end(),
-                               rowStringView.begin(),
-                               extract
-                              );
-                typeChart.setRow(rowName, category, rowStringView);
-            }
-
-            typeChart.show();
-
-            CsvService::freeRowBuffer(rowBuffer);
+            assertTypeChartDataComplete(csvHandle);
+            transferToTypeChart(csvHandle);
             CsvService::freeCsvData(csvHandle);
-
         }
         catch (const EngineError& e)
         {
             eb.append(e);
         }
     }
+
+    // ---------------------------------------------------------------------- //
+    // pokemon definition
+
+    std::unordered_map<std::string, size_t> assertPokemonDatabaseDataComplete(
+        const std::filesystem::__cxx11::path& pokemonDefinitionFile,
+        ICsvService::CsvHandle                handle,
+        ICsvService::RowData                  rowBuffer,
+        EngineBase::ErrorBuffer&              eb
+    )
+    {
+        bool resultValid = true;
+        std::unordered_map<std::string, size_t> result;
+        CsvService::getRow(handle, rowBuffer, 0);
+        std::span<ICsvService::CellData> rowView(
+            rowBuffer.data,
+            rowBuffer.data + rowBuffer.size
+        );
+
+        for (const std::string_view searchTerm :
+             {
+                 PokemonDatabase::SPECIES,
+                 PokemonDatabase::HP,
+                 PokemonDatabase::ATK,
+                 PokemonDatabase::DEF,
+                 PokemonDatabase::SPC,
+                 PokemonDatabase::SPD,
+                 PokemonDatabase::TYPE1,
+                 PokemonDatabase::TYPE2,
+                 PokemonDatabase::EXPYIELD,
+                 PokemonDatabase::EXPGROUP
+             })
+        {
+            const auto equals = [searchTerm](const ICsvService::CellData cell)
+            {
+                return searchTerm == cell.data;
+            };
+
+            auto it = std::find_if(rowView.begin(), rowView.end(), equals);
+            auto id = std::distance(rowView.begin(), it);
+
+            if (it == rowView.end())
+            {
+                eb.append(ApiStatusCode::INVALID_USER_INPUT,
+                          "In pokemon definition file '"s + pokemonDefinitionFile.c_str() + "': "
+                          "Missing Column header '" +  searchTerm.data() + "'"
+                         );
+                resultValid = false;
+            }
+            else
+            {
+                result.try_emplace(searchTerm.data(), id);
+            }
+        }
+
+        if (!resultValid)
+        {
+            result.clear();
+        }
+        return result;
+    }
+
+    void transferToPokemonDatabase(
+        const std::filesystem::__cxx11::path&         pokemonDefinitionFile,
+        ICsvService::CsvHandle                        handle,
+        ICsvService::RowData                          rowBuffer,
+        const std::unordered_map<std::string, size_t> columnNames,
+        EngineBase::ErrorBuffer&                      eb
+    )
+    {
+        const size_t rowCount = CsvService::getRowCount(handle);
+        std::string_view field;
+
+        const auto extractStringAndRememberField = [&field, &columnNames](
+                                                       const std::string_view fieldName,
+                                                       const ICsvService::RowData rowBuffer
+                                                   )
+        {
+            field = fieldName;
+            const auto it = columnNames.find(fieldName.data());
+            const size_t column = it->second;
+            const ICsvService::CellData cell = rowBuffer.data[column];
+            return cell.data;
+        };
+
+        for (size_t row = 1; row < rowCount; ++row)
+        {
+            try
+            {
+                CsvService::getRow(handle, rowBuffer, row);
+
+                const std::string species = extractStringAndRememberField(PokemonDatabase::SPECIES, rowBuffer);
+                PokemonDatabaseEntry entry;
+
+                entry.HP       =                  std::stoi(extractStringAndRememberField(PokemonDatabase::HP,       rowBuffer));
+                entry.ATK      =                  std::stoi(extractStringAndRememberField(PokemonDatabase::ATK,      rowBuffer));
+                entry.DEF      =                  std::stoi(extractStringAndRememberField(PokemonDatabase::DEF,      rowBuffer));
+                entry.SPC      =                  std::stoi(extractStringAndRememberField(PokemonDatabase::SPC,      rowBuffer));
+                entry.SPD      =                  std::stoi(extractStringAndRememberField(PokemonDatabase::SPD,      rowBuffer));
+                entry.expYield =                  std::stoi(extractStringAndRememberField(PokemonDatabase::EXPYIELD, rowBuffer));
+                entry.expGroup = getExperienceGroupFromName(extractStringAndRememberField(PokemonDatabase::EXPGROUP, rowBuffer));
+
+                entry.type.primary =  extractStringAndRememberField(PokemonDatabase::TYPE1, rowBuffer);
+                const std::string secondary = extractStringAndRememberField(PokemonDatabase::TYPE2, rowBuffer);
+                if (!secondary.empty())
+                {
+                    entry.type.secondary = secondary;
+                }
+
+                pokemonDatabase.addSpecies(species, entry);
+
+            }
+            catch (const EngineError& e)
+            {
+                eb.append(e);
+            }
+            catch (const std::invalid_argument&)
+            {
+                eb.append(ApiStatusCode::INVALID_USER_INPUT,
+                          "In pokemon definition file '"s + pokemonDefinitionFile.c_str() + "': "
+                          "Invalid Argument in line "s + std::to_string(row + 1) + " "
+                          "in field '" + field.data() + "'"
+                         );
+            }
+            catch (const std::out_of_range&)
+            {
+                eb.append(ApiStatusCode::INVALID_USER_INPUT,
+                          "In pokemon definition file '"s + pokemonDefinitionFile.c_str() + "': "
+                          "Invalid Argument in line "s + std::to_string(row + 1) + " "
+                          "in field '" + field.data() + "'"
+                         );
+            }
+        }
+    }
+
+    void loadAndRegisterPokemon(const std::filesystem::__cxx11::path& pokemonDefinitionFile, EngineBase::ErrorBuffer& eb)
+    {
+        try
+        {
+            LoggerService::traceF("  ... loading pokemon definition from '{}'", pokemonDefinitionFile.c_str());
+
+            ICsvService::CsvHandle csvHandle = CsvService::readCsvData(pokemonDefinitionFile, ICsvService::CsvOptions{});
+            ICsvService::RowData   rowBuffer = CsvService::reserveRowBuffer(csvHandle);
+
+            const std::unordered_map<std::string, size_t> columnNames =
+                assertPokemonDatabaseDataComplete(
+                    pokemonDefinitionFile,
+                    csvHandle,
+                    rowBuffer,
+                    eb
+                );
+            if (!columnNames.empty())
+            {
+                transferToPokemonDatabase(
+                    pokemonDefinitionFile,
+                    csvHandle,
+                    rowBuffer,
+                    columnNames,
+                    eb
+                );
+            }
+
+            CsvService::freeRowBuffer(rowBuffer);
+            CsvService::freeCsvData(csvHandle);
+        }
+        catch (const EngineError& e)
+        {
+            eb.append(e);
+        }
+    }
+
 } // namespace SimulationMode
